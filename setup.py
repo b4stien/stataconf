2fbff86d4873f4fd5ae86d2228386d8032e7f1e2
Bastien Gandouet <bastien@ouihelp.fr>
2023-08-10 21:31:49 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+
+from setuptools import setup
+from setuptools.command.build_py import build_py
+
+
+class CustomBuildPy(build_py):
+    def _get_package_data_output_mapping(self):
+        platform = os.environ["PLAT_NAME"]
+        print(f"platform is {platform}")
+
+        if platform == "macosx_11_0_arm64":
+            must_ends_with = "arm64"
+        elif platform == "manylinux2014_x86_64":
+            must_ends_with = "amd64"
+        else:
+            raise Exception("Unknown platform")
+
+        return [
+            v
+            for v in build_py._get_package_data_output_mapping(self)
+            if v[0].endswith(must_ends_with)
+        ]
+
+
+setup(cmdclass={"build_py": CustomBuildPy})
